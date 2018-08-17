aa394b00861c55002d90d3756c17b399ee0f8d90
osdnk <micosa97@gmail.com>
2018-08-17 22:05:47 +0200

    techs

--- src/technologies/Technologies.js
+++ src/technologies/Technologies.js
@@ -1,6 +1,30 @@
 import * as React from 'react';
+import Page from '../PageWrapper/Page';
 
 export default () =>
-  <div>
-    here will be work info some day
-  </div>
+  <Page>
+    <h2>Technologies</h2>
+    <h3>I know professionally</h3>
+    <p>
+      <li>React Native (with love), React, JS</li>
+    </p>
+    <h3>I know fine</h3>
+    <p>
+      <li>Redux, TS, Android Java, Objective-C</li>
+    </p>
+    <h3>I wish to know better</h3>
+    <p>
+      <li>Haskell, Coq, C, C++, Python, Ruby, Elixir, Erlang, anything related to backend</li>
+    </p>
+    <h3>I met</h3>
+    <p>
+      <li>C#, Python, Fortran</li>
+    </p>
+    <h3>I wish to know</h3>
+    <p>
+      <li>Rust, Go</li>
+    </p>
+
+
+  </Page>
+
