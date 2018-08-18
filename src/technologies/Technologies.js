46eb6645111f51a7f134e9324c3841eed8b03692
osdnk <micosa97@gmail.com>
2018-08-18 17:53:56 +0200

    remove li

--- src/technologies/Technologies.js
+++ src/technologies/Technologies.js
@@ -4,27 +4,25 @@ import Page from '../PageWrapper/Page';
 export default () =>
   <Page>
     <h2>Technologies</h2>
-    <h3>I know professionally</h3>
-    <p>
-      <li>React Native (with love), React, JS</li>
-    </p>
-    <h3>I know fine</h3>
-    <p>
-      <li>Redux, TS, Android Java, Objective-C</li>
-    </p>
-    <h3>I wish to know better</h3>
-    <p>
-      <li>Haskell, Coq, C, C++, Python, Ruby, Elixir, Erlang, anything related to backend</li>
-    </p>
-    <h3>I met</h3>
-    <p>
-      <li>C#, Python, Fortran</li>
-    </p>
-    <h3>I wish to know</h3>
-    <p>
-      <li>Rust, Go</li>
-    </p>
-
-
+      <h3>I know professionally</h3>
+      <p>
+        React Native (with love), React, JS
+      </p>
+      <h3>I know fine</h3>
+      <p>
+        Redux, TS, Android Java, Objective-C
+      </p>
+      <h3>I wish to know better</h3>
+      <p>
+        Haskell, Coq, C, C++, Python, Ruby, Elixir, Erlang, anything related to backend
+      </p>
+      <h3>I met</h3>
+      <p>
+        C#, Python, Fortran
+      </p>
+      <h3>I wish to know</h3>
+      <p>
+        Rust, Go
+      </p>
   </Page>
 
